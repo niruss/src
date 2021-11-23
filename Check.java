fdb28f5f53772eb8bced40f44276b3592264bcc6
niruss <94335291+niruss@users.noreply.github.com>
2021-11-23 07:24:19 +0530

    Add files via upload

---
+++ Check.java
@@ -0,0 +1,51 @@
+package Perform;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Check
+ */
+public class Check extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Check() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		PrintWriter pw=response.getWriter();
+		DateFormat d=new SimpleDateFormat("dd/MM/yyyy");
+		Date d1=new Date();
+		pw.print("<h1>"+"Date:   "+d.format(d1)+"</h1>");
+		
+		InetAddress ip=InetAddress.getLocalHost();
+		pw.print("<h1>"+"IP Address:   "+ip.getHostAddress()+"</h1>");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		
+	
+	}
+
+}
